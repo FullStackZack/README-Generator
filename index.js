9a27e211346de8e58fd8f7ea9aeeae41d221caf2
Zack White <zhwhite18@yahoo.com>
2020-09-08 16:37:04 -0500

    added questions

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 
+inquirer.prompt([
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a brief description about your project:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please explain instructions for installation:"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Now state usage information:"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Fill out your 'Contributing' section:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license:",
+        choices: [
+            "MIT",
+            "Mozilla Public License 2.0",
+            "Apache License 2.0",
+            "Boost Software License 1.0",
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Unlicense"
+        ]
+    }
+])
